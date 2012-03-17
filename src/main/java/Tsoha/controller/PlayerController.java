2016ea3cb6550f5641a328ee5f1fffe9fd8f3006
Noemj <Noemj@Noemj-PC.(none)>
2012-03-18 00:56:41 +0200

    Minor fixes

---
+++ src/main/java/Tsoha/controller/PlayerController.java
@@ -0,0 +1,31 @@
+package tsoha.controller;
+
+import Tsoha.domain.Peli;
+import Tsoha.service.PeliService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PlayerController {
+ 
+    @Autowired
+    private PeliService peliService;
+    
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    public String list(Model model){
+        model.addAttribute("pelit", peliService.listAll());
+        return "list";
+    }
+    
+    @RequestMapping(value="/peli", method = RequestMethod.GET)
+    public String getPeli(Model model){
+        model.addAttribute("peli", new Peli());
+        model.addAttribute("teams", genreService.list());
+        return "player";
+    }
+}
+
